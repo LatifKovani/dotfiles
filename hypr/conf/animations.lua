abc4c6e4a1cdfe2a44b83055405e39cab19178a4
LatifKovani <latif.kovani.st@uni-gjilan.net>
2026-05-24 02:16:55 +0200

    Openning apps animation changed

--- hypr/conf/animations.lua
+++ hypr/conf/animations.lua
@@ -2,11 +2,13 @@
 
 -- Bezier curves
 hl.curve("myBezier", { type = "bezier", points = { { 0.05, 0.9 }, { 0.1, 1.05 } } })
+hl.curve("easeInOutQuart", { type = "bezier", points = { { 0.76, 0 }, { 0.24, 1 } } })
+hl.curve("easy", { type = "spring", mass = 2, stiffness = 71.2633, dampening = 15.8273644 })
 
 -- Animations
 hl.animation({ leaf = "windows", enabled = true, speed = 7, bezier = "myBezier" })
-hl.animation({ leaf = "windowsIn", enabled = true, speed = 7, bezier = "myBezier", style = "gnomed" })
-hl.animation({ leaf = "windowsOut", enabled = true, speed = 7, bezier = "myBezier", style = "slide" })
+hl.animation({ leaf = "windowsIn", enabled = true, speed = 4.5, spring = "easy", style = "popin 87%" })
+hl.animation({ leaf = "windowsOut", enabled = true, speed = 7, bezier = "easeInOutQuart", style = "slide" })
 hl.animation({ leaf = "border", enabled = true, speed = 10, bezier = "default" })
 hl.animation({ leaf = "borderangle", enabled = true, speed = 8, bezier = "default" })
 hl.animation({ leaf = "fade", enabled = true, speed = 7, bezier = "default" })
